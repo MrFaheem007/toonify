d0e36cda2bed313c0a1577324d284061ae32e357
MrFaheem007 <100991256+MrFaheem007@users.noreply.github.com>
2022-05-17 10:25:16 -0700

    Update model.js

--- model.js
+++ model.js
@@ -1,7 +1,7 @@
 tf.setBackend('wasm').then(() => runModel())
 
 const APP = {
-  model: null, size: 600,
+  model: null, size: 500,
   source: document.getElementById('input'),
   canvas: document.getElementById('output'),
   status: document.getElementById('status'),
